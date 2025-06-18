74a653842426210430fc40618ad4cf36863e1ba5
Hanna19 <hannasisay05@gmail.com>
2025-06-18 13:33:38 +0300

    Load the data

--- project_code/train_svm_sentiment.py
+++ project_code/train_svm_sentiment.py
@@ -9,3 +9,8 @@ from sklearn.metrics import classification_report, confusion_matrix
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+# Load data
+df = pd.read_csv(r'datasets\sentiment_analysis\ethiopian_airlines_overall_sentiment.csv')
+X = df['review_comment']
+y = df['overall_sentiment']
+
